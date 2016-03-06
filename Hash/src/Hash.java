6ac208fe56e3ee9de04e7b48cd3e310f3f6df556
Luís Felipe <lftomazini@wifi177-038.bucknell.edu>
2016-03-06 17:01:10 -0500

    Hash started

---
+++ Hash/src/Hash.java
@@ -0,0 +1,19 @@
+
+/**
+ * An implementation of a hash function
+ */
+public class Hash {
+
+    /*
+     NOTE that the indexing of the characters in a Java string is reversed
+     from what we used in lecture.
+     */
+    public static int hash(String inStr, int radix, int modulus) {
+        int h = 0;
+        for (int i = 0; i < inStr.length() - 1; i++) {
+            h = ((h * radix + (int) inStr.charAt(i)) % modulus);
+        }
+
+        return h;
+    }
+}
---
+++ Hash/src/TestHash.java
@@ -0,0 +1,52 @@
+
+/**
+ * A class to test hash function variations Requires two integers to be provided
+ * on the command line
+ */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TestHash {
+
+    public static void main(String[] args) {
+
+        try {
+            //int radix = Integer.parseInt(args[0]);
+            //int modulus = Integer.parseInt(args[1]);
+            //File inFile = new File(args[2]);
+            int radix = 128;
+            int modulus = 127;
+            File inFile = new File("src/5lw.dat");
+            Scanner in = new Scanner(inFile);
+            String inStr;
+            int[] hashValues = new int[modulus];
+
+            System.out.println();
+            System.out.println("Using radix " + radix + " and modulus "
+                               + modulus
+                               + ".");
+            System.out.println();
+            System.out.println("  Input  |  hash value");
+            System.out.println("----------------------");
+
+            int hash;
+            while (in.hasNext()) {
+                inStr = in.next();
+                hash = Hash.hash(inStr, radix, modulus);
+                System.out.printf("%7s  |%8d\n", inStr, hash);
+                hashValues[hash]++;
+            }
+
+            System.out.println("\n");
+
+            for (int i = 0; i < hashValues.length; i++) {
+                System.out.print(hashValues[i] + ",");
+            }
+            System.out.println("\n");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("ERROR: File not found." + e);
+        }
+    }
+}

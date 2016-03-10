7eabc1f0942a5d58c137c6fd316c5c89c29afb6c
lftomazini <lftomazini@wifi177-038.bucknell.edu>
2016-03-10 15:33:34 -0500

    Assignment 3, complete

--- Hash/src/Hash.java
+++ Hash/src/Hash.java
@@ -3,11 +3,6 @@
  * An implementation of a hash function
  */
 public class Hash {
-
-    /*
-     NOTE that the indexing of the characters in a Java string is reversed
-     from what we used in lecture.
-     */
     public static int hash(String inStr, int radix, int modulus) {
         int h = 0;
         for (int i = 0; i < inStr.length(); i++) {
